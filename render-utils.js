066286b03551a53c63892bf5845e48e22b3b388f
benni maxon <benni.maxon@gmail.com>
2022-11-09 17:55:17 -0800

    built renderBeanie() function

---
+++ render-utils.js
@@ -0,0 +1,26 @@
+export function renderBeanie(beanie) {
+    const li = document.createElement('li');
+    li.classList.add('card');
+
+    const img = document.createElement('img');
+    img.src = beanie.image;
+    img.alt = beanie.title;
+
+    const content = document.createElement('div');
+    content.classList.add('content');
+
+    const h2 = document.createElement('h2');
+    h2.textContent = beanie.title;
+
+    const attributes = document.createElement('p');
+    attributes.classList.add('attributes');
+
+    const animal = document.createElement('span');
+    animal.textContent = beanie.animal;
+
+    const subtheme = document.createElement('span');
+    subtheme.textContent = beanie.subtheme;
+
+    const astroSign = document.createElement('span');
+    astroSign.textContent = beanie.astroSign;
+}

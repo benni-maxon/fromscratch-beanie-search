b7d5bf85f80ca5987d77408aaa81e8da44e7cd10
Grey Altr <grey.sifr@gmail.com>
2022-11-10 12:47:34 -0800

    grabbed DOM elements, set states, started window.addEventListener, and completed findBeanies and displayBeanies functions

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 /* Imports */
-
+import { getAstroSigns, getBeanies } from './fetch-utils.js';
+import { renderBeanie } from './render-utils.js';
 /* Get DOM Elements */
+const searchForm = document.querySelector('#search-form');
+const beanieList = document.querySelector('#beanie-list');
+const astroSignSelect = document.querySelector('#astro-sign-select');
 
 /* State */
 
+let beanies = [];
+let astroSigns = [];
+
 /* Events */
+
+window.addEventListener('load', async () => {});
+
+async function findBeanies(astroSign) {
+    const response = await getAstroSigns();
+    beanies = response.data;
+    displayBeanies();
+}
 
 /* Display Functions */
 
+function displayBeanies() {
+    beanieList.innerHTML = '';
+
+    for (let beanie of beanies) {
+        const beanieEl = renderBeanie(beanie);
+        beanieList.append(beanieEl);
+    }
+}
 // (don't forget to call any display functions you want to run on page load!)
